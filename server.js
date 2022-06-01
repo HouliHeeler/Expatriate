7e8a1b17e4b5d6a1fa9f3f0c875658540091a518
HouliHeeler <luke_houlihan@hotmail.com>
2022-05-31 21:04:45 -0700

    set up mongodb

--- server.js
+++ server.js
@@ -3,18 +3,28 @@ const { append } = require('express/lib/response')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+//Set up Database through MongoDB
+MongoClient.connect('mongodb+srv://Houli:Be%40n8678@cluster0.skyx0.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Time is yours and yours alone')
+        const db = client.db('Expatriate')
+        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(cors())
+        app.listen(3000,() => {
+            console.log('The Servant is always listening...')
+        })
+        app.get('/', (req, res) => {
+            res.sendFile(__dirname + '/index.html')
+        })
+        app.post('/quotes', (req,res) => {
+            console.log(req.body)
+        })
 })
+.catch(console.error)
 
-app.listen(3000,() => {
-    console.log('The Servant is always listening...')
-})
 
-app.post('/quotes', (req,res) => {
-    console.log(req.body)
-})
+
+
+
