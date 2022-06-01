5fc51a4d8cb5d473f70ba4d0d2f2129c1348cc07
HouliHeeler <luke_houlihan@hotmail.com>
2022-05-31 21:31:32 -0700

    work on post/get functionality

--- server.js
+++ server.js
@@ -10,16 +10,32 @@ MongoClient.connect('mongodb+srv://Houli:Be%40n8678@cluster0.skyx0.mongodb.net/?
     .then(client => {
         console.log('Time is yours and yours alone')
         const db = client.db('Expatriate')
+        const countries = db.collection('countries')
+
+        app.set('view engine', 'ejs')
+
         app.use(bodyParser.urlencoded({ extended: true }))
+
         app.use(cors())
+
         app.listen(3000,() => {
             console.log('The Servant is always listening...')
         })
+
         app.get('/', (req, res) => {
-            res.sendFile(__dirname + '/index.html')
+            db.collection('countries').find().toArray()
+            .then(results => {
+                res.render('index.ejs', {countries: results})
+            })
+            .catch(error => console.error(error))
         })
-        app.post('/quotes', (req,res) => {
-            console.log(req.body)
+
+        app.post('/countries', (req,res) => {
+            countries.insertOne(req.body)
+            .then(result => {
+                res.redirect('/')
+            })
+            .catch(error => console.error(error))
         })
 })
 .catch(console.error)
